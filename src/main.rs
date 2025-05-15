ab4ab8681bc76808fc7049808316231e638fb491
Sean Edwards <hello@seanedwards.dev>
2025-05-16 00:27:52 +0100

    Create service module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod enums;
 mod models;
-mod weather_service;
+mod services;
 
 use std::env;
 use std::error::Error;
@@ -14,7 +14,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let city = &args[1];
 
-    let _ = weather_service::get_weather(city);
+    let _ = services::weather_service::get_weather(city);
 
     Ok(())
 }
