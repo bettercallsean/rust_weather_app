9d5ce7a49309c0db7bc2e953fe720e66f7c343f5
Sean Edwards <hello@seanedwards.dev>
2025-05-27 13:32:11 +0100

    Store weather forecast in json file

--- src/services/weather_service.rs
+++ src/services/weather_service.rs
@@ -2,18 +2,37 @@ use crate::models::location::{Location, Locations};
 use crate::models::weather::Weather;
 use chrono::Local;
 use std::error::Error;
+use std::fs::{self, File};
+use std::io::Write;
 use std::{env, process};
 
 pub async fn get_weather_report(city: &String) -> Result<(), Box<dyn Error>> {
-    let api_key = get_weather_api_key()?;
+    let weather = match get_stored_weather() {
+        Ok(weather) => {
+            let current_time = chrono::Local::now().timestamp();
 
-    let location = get_city_location(&api_key, city).await?;
+            if weather.name != *city || ((current_time - weather.dt) / 60) >= 10 {
+                get_weather(city).await?;
+            }
 
-    let weather = get_weather(&api_key, &location).await?;
+            weather
+        }
+        Err(_) => get_weather(city).await?,
+    };
 
-    print_weather(weather);
+    print_weather(&weather);
+
+    store_weather_result(&weather)?;
 
     Ok(())
+}
+
+async fn get_weather(city: &String) -> Result<Weather, Box<dyn Error>> {
+    let api_key = get_weather_api_key()?;
+    let location = get_city_location(&api_key, city).await?;
+    let weather = get_new_weather_forecast(&api_key, &location).await?;
+
+    Ok(weather)
 }
 
 fn get_weather_api_key() -> Result<String, Box<dyn Error>> {
@@ -25,7 +44,10 @@ fn get_weather_api_key() -> Result<String, Box<dyn Error>> {
     }
 }
 
-async fn get_weather(api_key: &String, location: &Location) -> Result<Weather, Box<dyn Error>> {
+async fn get_new_weather_forecast(
+    api_key: &String,
+    location: &Location,
+) -> Result<Weather, Box<dyn Error>> {
     let url = format!(
         "https://api.openweathermap.org/data/2.5/weather?lon={}&lat={}&appid={}&units=metric",
         location.lon, location.lat, api_key
@@ -36,7 +58,7 @@ async fn get_weather(api_key: &String, location: &Location) -> Result<Weather, B
     match response.error_for_status() {
         Ok(response) => {
             let json = response.text().await?;
-            let weather = parse_weather_json(json)?;
+            let weather = parse_weather_json(json.as_str())?;
             Ok(weather)
         }
         Err(error) => Err(Box::new(error)),
@@ -73,13 +95,13 @@ fn parse_location_json(location_json: String) -> Result<Location, Box<dyn Error>
     }
 }
 
-fn parse_weather_json(weather_json: String) -> Result<Weather, Box<dyn Error>> {
-    let weather: Weather = serde_json::from_str(&weather_json)?;
+fn parse_weather_json(weather_json: &str) -> Result<Weather, Box<dyn Error>> {
+    let weather: Weather = serde_json::from_str(weather_json)?;
 
     Ok(weather)
 }
 
-fn print_weather(weather: Weather) {
+fn print_weather(weather: &Weather) {
     println!("Weather for {}", weather.name);
     println!("{}", Local::now().format("%d/%m/%y %H:%M"));
 
@@ -94,4 +116,19 @@ fn print_weather(weather: Weather) {
 
     println!("Current temp: {:.0}ºC", weather.temperature.temp);
     println!("Feels like: {:.0}ºC", weather.temperature.feels_like);
+}
+
+fn store_weather_result(weather: &Weather) -> std::io::Result<()> {
+    let weather_json = serde_json::to_string(weather)?;
+    let mut file = File::create("weather.json")?;
+    write!(file, "{}", weather_json)?;
+
+    Ok(())
+}
+
+fn get_stored_weather() -> Result<Weather, Box<dyn Error>> {
+    let weather_json = fs::read_to_string("weather.json")?;
+    let weather: Weather = serde_json::from_str(&weather_json)?;
+
+    Ok(weather)
 }
